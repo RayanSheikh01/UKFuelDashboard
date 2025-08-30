7d9a0fc97feb08399fb11f2a852c8941b6b22134
Rayan Sheikh <w2094027@westminster.ac.uk>
2025-08-31 00:59:39 +0100

    Add company fuel prices and implement using them for bar chart in app.py

--- app.py
+++ app.py
@@ -11,23 +11,69 @@ app.layout = dbc.Container([
     ], className="my-3")
 ], fluid=True)
 
-data = pd.read_csv('uk_fuel_prices.csv')
+asdaData = pd.read_json('AsdaFuelPrices.json')
+BPData = pd.read_json('BPFuelPrices.json')
+EssoData = pd.read_json('EssoFuelPrices.json')
+ShellData = pd.read_json('ShellFuelPrices.json')
+MFGData = pd.read_json('MFGFuelPrices.json')
 
-datatable = dbc.Table.from_dataframe(data, striped=True, bordered=True, hover=True)
-app.layout.children.append(datatable)
+asdaData = asdaData['stations']
+asdaData = asdaData[0]
+asdaPrices = {'E10': asdaData['prices']['E10'], 'B7': asdaData['prices']['B7']}
+asdaData = {'brand': 'Asda', 'prices': asdaPrices}
+asdaData = pd.json_normalize(asdaData)
 
-line_chart = dcc.Graph(
+BPData = BPData['stations']
+BPData = BPData[0]
+BPPrices = {'E10': BPData['prices']['E10'], 'B7': BPData['prices']['B7']}
+BPData = {'brand': 'BP', 'prices': BPPrices}
+BPData = pd.json_normalize(BPData)
+
+EssoData = EssoData['stations']
+EssoData = EssoData[0]
+EssoPrices = {'E10': EssoData['prices']['E10'], 'B7': EssoData['prices']['B7']}
+EssoData = {'brand': 'Esso', 'prices': EssoPrices}
+EssoData = pd.json_normalize(EssoData)
+
+ShellData = ShellData['stations']
+ShellData = ShellData[0]
+ShellPrices = {'E10': ShellData['prices']['E10'], 'B7': ShellData['prices']['B7']}
+ShellData = {'brand': 'Shell', 'prices': ShellPrices}
+ShellData = pd.json_normalize(ShellData)
+
+MFGData = MFGData['stations']
+MFGData = MFGData[0]
+MFGPrices = {'E10': MFGData['prices']['E10'], 'B7': MFGData['prices']['B7']}
+MFGData = {'brand': 'MFG', 'prices': MFGPrices}
+MFGData = pd.json_normalize(MFGData)
+
+
+
+data = pd.concat([asdaData, BPData, EssoData, ShellData, MFGData], ignore_index=True)
+
+
+bar_chart = dcc.Graph(
     figure={
         'data': [
-            {'x': data['date'], 'y': data['unleaded'], 'type': 'line', 'name': 'Unleaded'},
-            {'x': data['date'], 'y': data['diesel'], 'type': 'line', 'name': 'Diesel'},
-        ],
+            {'x': data['brand'], 'y': data['prices.E10'], 'type': 'bar', 'name': 'Unleaded Prices'},
+            {'x': data['brand'], 'y': data['prices.B7'], 'type': 'bar', 'name': 'Diesel Prices'}
+        ]
+        ,
+
         'layout': {
-            'title': 'Fuel Prices Over Time'
+            'title': {'text': 'Fuel Prices by Brand', 'x': 0.5, 'xanchor': 'center'},
+            'xaxis': {'title': {'text': 'Brand', 'standoff': 10}},
+            'yaxis': {'title': {'text': 'Price (Pence)', 'standoff': 10}},
         }
     }
 )
-app.layout.children.append(line_chart)
+
+
+app.layout.children.append(bar_chart)
+
+# app.layout.children.append(dbc.Row([
+#     dbc.Col(bar_chart, width=12)
+# ], className="my-3"))
 
 # Run the server
 if __name__ == "__main__":
