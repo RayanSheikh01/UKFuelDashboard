031b470db502f265980750a66e7fbb016148fd4d
Rayan Sheikh <w2094027@westminster.ac.uk>
2025-08-31 00:04:11 +0100

    Add line chart

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 import pandas as pd
 
@@ -15,6 +15,19 @@ data = pd.read_csv('uk_fuel_prices.csv')
 
 datatable = dbc.Table.from_dataframe(data, striped=True, bordered=True, hover=True)
 app.layout.children.append(datatable)
+
+line_chart = dcc.Graph(
+    figure={
+        'data': [
+            {'x': data['date'], 'y': data['unleaded'], 'type': 'line', 'name': 'Unleaded'},
+            {'x': data['date'], 'y': data['diesel'], 'type': 'line', 'name': 'Diesel'},
+        ],
+        'layout': {
+            'title': 'Fuel Prices Over Time'
+        }
+    }
+)
+app.layout.children.append(line_chart)
 
 # Run the server
 if __name__ == "__main__":
